2210ae677d02de6abaf893dd97654a0a01fb10c2
Mokded Maryam <80172649+maryam-mokded@users.noreply.github.com>
2021-11-27 09:25:45 +0100

    Final commit

--- lib/TodosScreen.dart
+++ lib/TodosScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'DetailScreen.dart';
 import 'todo.dart';
 class TodosScreen extends StatelessWidget {
-  // Requiring the list of todos.
   const TodosScreen({Key? key, required this.todos}) : super(key: key);
 
   final List<Todo> todos;
@@ -13,15 +12,11 @@ class TodosScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Todos'),
       ),
-      //passing in the ListView.builder
       body: ListView.builder(
             itemCount: todos.length,
             itemBuilder: (context, index) {
               return ListTile(
                 title: Text(todos[index].title),
-                // When a user taps the ListTile, navigate to the DetailScreen.
-                // Notice that you're not only creating a DetailScreen, you're
-                // also passing the current todo through to it.
                 onTap: () {
                   Navigator.push(
                     context,
